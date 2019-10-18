de9b926624dc0c022e84b83b1a38c80c0cedad49
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-18 00:25:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow
-  puts colors["R; red, O: orange, Y: yellow, G: Green, B: blue, O: indigo, V: violet"]
+  colors = ["R; red, O: orange, Y: yellow, G: Green, B: blue, O: indigo, V: violet"]
 end
