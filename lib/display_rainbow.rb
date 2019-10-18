3203ff4bc8a1b5aa6fdd62a8f1997b7fbfd38e48
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-18 00:27:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,3 +2,5 @@
 def display_rainbow
   colors = ["R; red, O: orange, Y: yellow, G: Green, B: blue, O: indigo, V: violet"]
 end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
